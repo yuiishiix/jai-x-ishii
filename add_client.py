36c61f39caa8258b81f6f2629dd26ebb3fb074fb
ishii <yuichiroishiio@gmail.com>
2025-01-18 15:19:22 +0800

    Update add_client.py

--- add_client.py
+++ add_client.py
@@ -7,7 +7,6 @@ def add_client():
         email = input("Enter Your Email: ")
 
 
-        # Generate a unique account number
         client_id = len(clients) + 1001
         clients[client_id] = {"name": name, "email": email }
         print(f"Client added successfully! Your account number is {client_id}.")
